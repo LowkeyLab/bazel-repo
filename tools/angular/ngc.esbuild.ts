44705ac293018290a3f8472324f7d048babfeab3
tacascer <trandangtrithanh2000@gmail.com>
2025-10-13 12:49:49 +0100

    something

--- tools/angular/ngc.esbuild.ts
+++ tools/angular/ngc.esbuild.ts
@@ -13,48 +13,48 @@
 
 import { transformFileAsync } from '@babel/core';
 import {
-  ConsoleLogger,
-  NodeJSFileSystem,
-  LogLevel,
+    ConsoleLogger,
+    NodeJSFileSystem,
+    LogLevel,
 } from '@angular/compiler-cli';
 import { createEs2015LinkerPlugin } from '@angular/compiler-cli/linker/babel';
 
 const linkerBabelPlugin = createEs2015LinkerPlugin({
-  fileSystem: new NodeJSFileSystem(),
-  logger: new ConsoleLogger(LogLevel.warn),
-  unknownDeclarationVersionHandling: 'error',
-  // Must enable JIT for unit tests
-  // TODO: would be ideal to only set this for tests
-  linkerJitMode: true,
-  // Workaround for https://github.com/angular/angular/issues/42769 and https://github.com/angular/angular-cli/issues/22647.
-  sourceMapping: false,
+    fileSystem: new NodeJSFileSystem(),
+    logger: new ConsoleLogger(LogLevel.warn),
+    unknownDeclarationVersionHandling: 'error',
+    // Must enable JIT for unit tests
+    // TODO: would be ideal to only set this for tests
+    linkerJitMode: true,
+    // Workaround for https://github.com/angular/angular/issues/42769 and https://github.com/angular/angular-cli/issues/22647.
+    sourceMapping: false,
 });
 
 const ngLinkerPlugin = {
-  name: 'ng-linker-esbuild',
-  setup(build: any) {
-    build.onLoad({ filter: /node_modules/ }, async (args: any) => {
-      const filePath = args.path;
-      const transformResult = await transformFileAsync(filePath, {
-        filename: filePath,
-        filenameRelative: filePath,
-        plugins: [linkerBabelPlugin],
-        sourceMaps: 'inline',
-        compact: false,
-      });
+    name: 'ng-linker-esbuild',
+    setup(build: any) {
+        build.onLoad({ filter: /node_modules/ }, async (args: any) => {
+            const filePath = args.path;
+            const transformResult = await transformFileAsync(filePath, {
+                filename: filePath,
+                filenameRelative: filePath,
+                plugins: [linkerBabelPlugin],
+                sourceMaps: 'inline',
+                compact: false,
+            });
 
-      if (!transformResult) {
-        throw new Error('Babel NG Linker error');
-      }
+            if (!transformResult) {
+                throw new Error('Babel NG Linker error');
+            }
 
-      return { contents: transformResult.code };
-    });
-  },
+            return { contents: transformResult.code };
+        });
+    },
 };
 
 export default {
-  // Ensure only [m]js is consumed. Any typescript should be precompiled
-  // and not consumed by esbuild.
-  resolveExtensions: ['.mjs', '.js'],
-  plugins: [ngLinkerPlugin],
+    // Ensure only [m]js is consumed. Any typescript should be precompiled
+    // and not consumed by esbuild.
+    resolveExtensions: ['.mjs', '.js'],
+    plugins: [ngLinkerPlugin],
 };
