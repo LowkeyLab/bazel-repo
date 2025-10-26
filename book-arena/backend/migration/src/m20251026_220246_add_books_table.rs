56dd552bef6234d06058fdc9332db83ed87441cf
tacascer <trandangtrithanh2000@gmail.com>
2025-10-26 22:46:52 +0000

    feat(migration): add books and user-to-book tables with indices in migration feat(build): create BUILD.bazel for backend server and define Rust library feat: add initial lib.rs file for backend server

--- book-arena/backend/migration/src/m20251026_220246_add_books_table.rs
+++ book-arena/backend/migration/src/m20251026_220246_add_books_table.rs
@@ -12,13 +12,78 @@ impl MigrationTrait for Migration {
                     .table(Book::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Book::Id)
+                        ColumnDef::new(Books::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Book::OpenLibraryWorkId).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Books::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Books::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(BookToOpenLibraryWorkId::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(BookToOpenLibraryWorkId::BookId)
+                            .integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(BookToOpenLibraryWorkId::OpenLibraryWorkId)
+                            .string()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(UserToBook::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(UserToBook::UserId).integer().not_null())
+                    .col(ColumnDef::new(UserToBook::BookId).integer().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create(
+                Index::create()
+                    .name("idx-user_to_book-user_id")
+                    .table(UserToBook::Table)
+                    .col(UserToBook::UserId)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create(
+                Index::create()
+                    .name("idx-user_to_book-book_id")
+                    .table(UserToBook::Table)
+                    .col(UserToBook::BookId)
                     .to_owned(),
             )
             .await?;
@@ -28,13 +93,34 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Book::Table).to_owned())
-            .await
+            .drop_table(Table::drop().table(UserToBook::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Books::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await?;
+
+        Ok(())
     }
 }
 
 #[derive(DeriveIden)]
-enum Book {
+enum Users {
+    Table,
+    Id,
+    Name,
+}
+
+enum UserToBook {
+    Table,
+    UserId,
+    BookId,
+}
+
+#[derive(DeriveIden)]
+enum Books {
     Table,
     Id,
     OpenLibraryWorkId,
