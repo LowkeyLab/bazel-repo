00e5df0d11c1c1d492f951f38e47ae6252852eda
tacascer <trandangtrithanh2000@gmail.com>
2025-11-11 01:50:22 +0000

    feat: Add Ktor server status pages dependency and implement error handling in routing

--- guess_the_word/src/Routing.kt
+++ guess_the_word/src/Routing.kt
@@ -2,13 +2,20 @@ package io.lowkeylabs
 
 import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
+        install(StatusPages) {
+            exception<IllegalStateException> { call, cause ->
+                call.respondText("App in illegal state as ${cause.message}")
+            }
+        }
         staticResources("/content", "mycontent")
         get("/") {
             call.respondText("Hello World!")
@@ -17,6 +24,9 @@ fun Application.configureRouting() {
             val text = "<h1>Hello From Ktor</h1>"
             val type = ContentType.parse("text/html")
             call.respondText(text, type)
+        }
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
         }
     }
 }
