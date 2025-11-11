b52da70feb33773188d5c178aa5bddfaf66a6133
tacascer <trandangtrithanh2000@gmail.com>
2025-11-11 21:35:15 +0000

    feat: Add task management UI and implement task addition functionality

--- guess_the_word/src/Routing.kt
+++ guess_the_word/src/Routing.kt
@@ -1,20 +1,109 @@
 package io.lowkeylabs
 
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import io.lowkeylabs.model.tasks
+import io.lowkeylabs.model.Priority
+import io.lowkeylabs.model.Task
+import io.lowkeylabs.model.TaskRepository
 import io.lowkeylabs.model.tasksAsTable
 
 fun Application.configureRouting() {
     routing {
-        get("/tasks") {
-            call.respondText(
-                contentType = ContentType.parse("text/html"),
-                text = tasks.tasksAsTable(),
-            )
+        staticResources("/task-ui", "task-ui")
+
+        route("/tasks") {
+            get {
+                val tasks = TaskRepository.allTasks()
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable(),
+                )
+            }
+
+            get("/byName/{taskName}") {
+                val name = call.parameters["taskName"]
+                if (name == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+
+                val task = TaskRepository.taskByName(name)
+                if (task == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = listOf(task).tasksAsTable(),
+                )
+            }
+
+            get("/byPriority/{priority?}") {
+                val priorityAsText = call.parameters["priority"]
+                if (priorityAsText == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+
+                try {
+                    val priority = Priority.valueOf(priorityAsText)
+                    val tasks = TaskRepository.tasksByPriority(priority)
+
+                    if (tasks.isEmpty()) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+
+                    call.respondText(
+                        contentType = ContentType.parse("text/html"),
+                        text = tasks.tasksAsTable(),
+                    )
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+
+            post {
+                val formContent = call.receiveParameters()
+                val params =
+                    Triple(
+                        formContent["name"] ?: "",
+                        formContent["description"] ?: "",
+                        formContent["priority"] ?: "",
+                    )
+
+                if (params.toList().any { it.isEmpty() }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+
+                try {
+                    val priority = Priority.valueOf(params.third)
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority,
+                        ),
+                    )
+
+                    call.respond(HttpStatusCode.NoContent)
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
         }
     }
 }
--- guess_the_word/test/ApplicationTest.kt
+++ guess_the_word/test/ApplicationTest.kt
@@ -1,9 +1,14 @@
 package io.lowkeylabs
 
 import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
+import io.ktor.http.formUrlEncode
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertContains
@@ -11,28 +16,71 @@ import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() =
+    fun tasksCanBeFoundByPriority() =
         testApplication {
             application {
                 module()
             }
-            val response = client.get("/")
+
+            val response = client.get("/tasks/byPriority/Medium")
+            val body = response.bodyAsText()
 
             assertEquals(HttpStatusCode.OK, response.status)
-            assertEquals("Hello World!", response.bodyAsText())
+            assertContains(body, "Mow the lawn")
+            assertContains(body, "Paint the fence")
         }
 
     @Test
-    fun testNewEndpoint() =
+    fun invalidPriorityProduces400() =
         testApplication {
             application {
                 module()
             }
 
-            val response = client.get("/test1")
+            val response = client.get("/tasks/byPriority/Invalid")
+            assertEquals(HttpStatusCode.BadRequest, response.status)
+        }
 
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertEquals("html", response.contentType()?.contentSubtype)
-            assertContains(response.bodyAsText(), "Hello From Ktor")
+    @Test
+    fun unusedPriorityProduces404() =
+        testApplication {
+            application {
+                module()
+            }
+
+            val response = client.get("/tasks/byPriority/Vital")
+            assertEquals(HttpStatusCode.NotFound, response.status)
+        }
+
+    @Test
+    fun newTasksCanBeAdded() =
+        testApplication {
+            application {
+                module()
+            }
+
+            val response1 =
+                client.post("/tasks") {
+                    header(
+                        HttpHeaders.ContentType,
+                        ContentType.Application.FormUrlEncoded.toString(),
+                    )
+                    setBody(
+                        listOf(
+                            "name" to "swimming",
+                            "description" to "Go to the beach",
+                            "priority" to "Low",
+                        ).formUrlEncode(),
+                    )
+                }
+
+            assertEquals(HttpStatusCode.NoContent, response1.status)
+
+            val response2 = client.get("/tasks")
+            assertEquals(HttpStatusCode.OK, response2.status)
+            val body = response2.bodyAsText()
+
+            assertContains(body, "swimming")
+            assertContains(body, "Go to the beach")
         }
 }
