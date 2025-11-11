f8f891e8bec33da9da56f9333905eb368f4fa664
tacascer <trandangtrithanh2000@gmail.com>
2025-11-11 00:17:55 +0000

    feat: Add new endpoint and update routing for Ktor application

--- guess_the_word/src/Routing.kt
+++ guess_the_word/src/Routing.kt
@@ -1,14 +1,22 @@
 package io.lowkeylabs
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent")
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
