0de354668fefe80656ced597728734594ec8e636
tacascer <trandangtrithanh2000@gmail.com>
2025-11-11 00:48:55 +0000

    Refactor City and User services for improved readability and structure
    - Updated CitySchema.kt to enhance data class formatting and service structure.
    - Refactored database connection setup in Databases.kt for better readability.
    - Improved UserService in UsersSchema.kt with clearer function definitions and formatting.
    - Added ApplicationTest.kt for testing application endpoints and removed old ApplicationTests.kt.
    - Introduced ktlint wrapper script and updated BUILD.bazel to integrate ktlint for Kotlin formatting.

--- guess_the_word/src/Databases.kt
+++ guess_the_word/src/Databases.kt
@@ -12,12 +12,13 @@ import io.ktor.server.routing.routing
 import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabases() {
-    val database = Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "",
-    )
+    val database =
+        Database.connect(
+            url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+            user = "root",
+            driver = "org.h2.Driver",
+            password = "",
+        )
     val userService = UserService(database)
     routing {
         // Create user
