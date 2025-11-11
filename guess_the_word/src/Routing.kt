e1a331ea07d39fbf76325850442e76f4cfd585dd
tacascer <trandangtrithanh2000@gmail.com>
2025-11-11 02:23:06 +0000

    feat: Implement task management with new Task model and routing for tasks endpoint

--- guess_the_word/src/Routing.kt
+++ guess_the_word/src/Routing.kt
@@ -2,31 +2,19 @@ package io.lowkeylabs
 
 import io.ktor.http.ContentType
 import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.http.content.staticResources
-import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import io.lowkeylabs.model.tasks
+import io.lowkeylabs.model.tasksAsTable
 
 fun Application.configureRouting() {
     routing {
-        install(StatusPages) {
-            exception<IllegalStateException> { call, cause ->
-                call.respondText("App in illegal state as ${cause.message}")
-            }
-        }
-        staticResources("/content", "mycontent")
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        get("/test1") {
-            val text = "<h1>Hello From Ktor</h1>"
-            val type = ContentType.parse("text/html")
-            call.respondText(text, type)
-        }
-        get("/error-test") {
-            throw IllegalStateException("Too Busy")
+        get("/tasks") {
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable(),
+            )
         }
     }
 }
---
+++ guess_the_word/src/model/TaskRepository.kt
@@ -0,0 +1,9 @@
+package io.lowkeylabs.model
+
+val tasks =
+    mutableListOf(
+        Task("cleaning", "Clean the house", Priority.Low),
+        Task("gardening", "Mow the lawn", Priority.Medium),
+        Task("shopping", "Buy the groceries", Priority.High),
+        Task("painting", "Paint the fence", Priority.Medium),
+    )
