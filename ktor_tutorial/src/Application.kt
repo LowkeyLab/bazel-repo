fc04fd69d5df3081d7d7e4c530b3a49de6257fdb
Tim Tran <49887921+tacascer@users.noreply.github.com>
2025-11-16 21:46:10 +0000

    feat: do ktor tutorial (#157)

---
+++ ktor_tutorial/src/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import com.example.model.PostgresTaskRepository
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    val repository = PostgresTaskRepository()
+
+    configureSerialization(repository)
+    configureDatabases()
+    configureRouting()
+}
---
+++ ktor_tutorial/src/Routing.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/static", "static")
+    }
+}
