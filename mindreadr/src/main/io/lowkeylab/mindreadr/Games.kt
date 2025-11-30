a2a19479dc2df4ff283fb059504eb7b7de535d74
tacascer <trandangtrithanh2000@gmail.com>
2025-11-30 03:31:53 +0000

    feat: remove PlayerJoined message from OutgoingMessage class

--- mindreadr/src/main/io/lowkeylab/mindreadr/Games.kt
+++ mindreadr/src/main/io/lowkeylab/mindreadr/Games.kt
@@ -291,12 +291,6 @@ sealed class OutgoingMessage {
     ) : OutgoingMessage()
 
     @Serializable
-    @SerialName("player_joined")
-    data class PlayerJoined(
-        val player: Player,
-    ) : OutgoingMessage()
-
-    @Serializable
     @SerialName("player_left")
     data class PlayerLeft(
         val player: Player,
