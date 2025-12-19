a0a478fe4ae23983771075c876efc2057259e37d
Tim Tran <49887921+tacascer@users.noreply.github.com>
2025-12-19 21:14:40 +0000

    build: Setup go (#304)

---
+++ cowsay/cmd/hello/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"nmyk.io/cowsay"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	cowsay.Cow{}.Write(w, []byte("Hello, world!"), false)
+}
