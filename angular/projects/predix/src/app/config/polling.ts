7a7b849162de0beb402d29aba712b129b7f952e2
tacascer <trandangtrithanh2000@gmail.com>
2026-01-15 01:40:39 +0000

    test: add unit tests for ContestStatsComponent to verify rendering and data passing

--- angular/projects/predix/src/app/config/polling.ts
+++ angular/projects/predix/src/app/config/polling.ts
@@ -2,5 +2,5 @@
  * Shared polling configuration constants.
  */
 
-/** Default polling interval in milliseconds (2 seconds) */
+/** Default polling interval in milliseconds */
 export const DEFAULT_POLL_INTERVAL_MS = 1000;
