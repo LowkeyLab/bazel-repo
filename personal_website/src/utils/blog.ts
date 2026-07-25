33933b3a88d63d56a25e70960cd8e03ecd165518
tacascer <trandangtrithanh2000@gmail.com>
2026-07-25 01:47:03 +0100

    build(personal-website): manage TypeScript with Gazelle

--- personal_website/src/utils/blog.ts
+++ personal_website/src/utils/blog.ts
@@ -1,4 +1,4 @@
-export function calculateReadingTime(content) {
+export function calculateReadingTime(content: string) {
   const words = content.split(/\s+/).length;
   return Math.ceil(words / 200);
 }
